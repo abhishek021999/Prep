61a00c8545591c37d91b0c1236801b98ab8a7c7e
Abhishek Singh <aksingh171999@gmail.com>
2025-05-28 01:45:39 +0530

    adaded

--- FrontendMovie/src/Components/Home.jsx
+++ FrontendMovie/src/Components/Home.jsx
@@ -2,11 +2,11 @@ import MovieList from "../components/MovieList";
 import "../App.css";
 
 
-
+ // MovieList
 const Home = () => (
   <div className="container">
     <h1>🎬 Movie Listings</h1>
-  
+ 
    
     <MovieList />
   </div>
